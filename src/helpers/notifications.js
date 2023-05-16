67b52a8e00b3c2f183c5d34d8b614e1608047070
fakofbolan <116907511+fakofbolan@users.noreply.github.com>
2023-05-16 12:38:24 +0200

    sound notification fix

--- src/helpers/notifications.js
+++ src/helpers/notifications.js
@@ -1,3 +1,3 @@
 export const messageNotification =
-  "../../../src/assets/message_notification.mp3";
-export const userNotification = "../../../src/assets/user_notification.mp3";
+  "/message_notification.mp3";
+export const userNotification = "/user_notification.mp3";
