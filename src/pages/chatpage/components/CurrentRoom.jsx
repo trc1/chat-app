183a40e62902677ddf3e637bdc088347ca32ebb7
Dario Trcović <116907511+trc1@users.noreply.github.com>
2023-05-20 16:03:05 +0200

    fixed icon

--- src/pages/chatpage/components/CurrentRoom.jsx
+++ src/pages/chatpage/components/CurrentRoom.jsx
@@ -16,10 +16,10 @@ export const CurrentRoom = ({ getRoomOpen }) => {
       <h2>{user.room}</h2>
       <button className="room-toggle" onClick={handleRoomOpen}>
         <svg
+          className="room-opener-icon"
           width="15"
           height="10"
           viewBox="0 0 15 10"
-          fill="none"
           xmlns="http://www.w3.org/2000/svg"
         >
           <path
