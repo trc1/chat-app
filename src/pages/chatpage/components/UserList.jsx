a20962a115433f9965ec2b7d0fbf73cd0b204488
Dario Trcović <116907511+trc1@users.noreply.github.com>
2023-05-24 12:43:20 +0200

    sort user list

--- src/pages/chatpage/components/UserList.jsx
+++ src/pages/chatpage/components/UserList.jsx
@@ -8,22 +8,27 @@ export const UserList = () => {
     (member) => member.id !== drone.clientId
   );
 
+  console.log(onlineUsers);
   return (
     <ul className="online-users-container">
       <div className="online-user-header">Online:</div>
-      {onlineUsers.map((member) => (
-        <li className="online-user" key={member.id}>
-          <span className="online-user-avatar">
-            <img
-              className="online-user-img"
-              src={member.clientData.avatar}
-            ></img>
-          </span>
-          <span className="online-user-username">
-            {member.clientData.username}
-          </span>
-        </li>
-      ))}
+      {onlineUsers
+        .sort((a, b) =>
+          a.clientData.username.localeCompare(b.clientData.username)
+        )
+        .map((member) => (
+          <li className="online-user" key={member.id}>
+            <span className="online-user-avatar">
+              <img
+                className="online-user-img"
+                src={member.clientData.avatar}
+              ></img>
+            </span>
+            <span className="online-user-username">
+              {member.clientData.username}
+            </span>
+          </li>
+        ))}
     </ul>
   );
 };
